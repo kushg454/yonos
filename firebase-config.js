0a274dfdfad0f80979f7d1f45e169e9fb61bd7e7
kushg454 <155818974+kushg454@users.noreply.github.com>
2024-01-06 17:05:41 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBduOxA6U_as_eSenZeYgENgIbymqT21YI",
+  authDomain: "kjus-c0809.firebaseapp.com",
+  databaseURL: "https://kjus-c0809-default-rtdb.firebaseio.com",
+  projectId: "kjus-c0809",
+  storageBucket: "kjus-c0809.appspot.com",
+  messagingSenderId: "431856281924",
+  appId: "1:431856281924:web:7754bb034e846c99084f11"
+};
